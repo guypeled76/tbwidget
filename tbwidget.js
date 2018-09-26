d557f0ff2ea13850afbf75703f3a8ba771fa1aa6
Guy Peled <guypeled76@gmail.com>
2018-09-26 06:54:35 +0300

    Enhance item template

--- tbwidget.js
+++ tbwidget.js
@@ -16,25 +16,52 @@ class TBWidget {
         this.url = "http://api.taboola.com/1.0/json/" + publisherid + "/recommendations.get?" +
         "app.type=" +  apptype +
         "&app.apikey=" + apikey +
-        "&source.id=214321562187" +
-        "&source.url=http://www.site.com/videos/214321562187.html";
+        "&source.id=214321562187" + // Should be added as parameter ?
+        "&source.url=http://www.site.com/videos/214321562187.html"; // Should be document.location.href
 
     }
 
 
     /**
      * Register a TB widget container
-     * @param {string} container 
-     * @param {string} type 
-     * @param {integer} count 
+     * @param {string} container The widget container id.
+     * @param {string} type The widget item type. 
+     * @param {integer} count The widget max items count.
+     * @param {function} template The widget item template (optional)
      */
-    register(container, type, count) {
+    register(container, type, count, template) {
+
+        // Ensure there is a default item tempate
+        if(!template) template = this.buildItem;
+
+        // The widget info
         var info = {
             container:container,
             type:type,
-            count:count
+            count:count,
+            template:template
         };
         this.containers.push(info);
+    }
+
+    /**
+     * Build tb widgets
+     */
+    build() {
+        this.containers.forEach((info) => {
+            var div = document.getElementById(info.container); // O(1) time complexity
+            if(div) {
+                fetch(this.buildUrl(info)).then((response) => {
+                    response.json().then((data) => {
+                        this.buildWidget(div, data, info.template);
+                    }).catch((reason) =>{
+                        this.logError("Failed to parse widget '"+info.container+"' data", reason);
+                    });
+                }).catch((reason) => {
+                    this.logError("Failed to load widget '"+info.container+"'  data", reason);
+                });
+            }
+        });
     }
 
     /**
@@ -47,53 +74,52 @@ class TBWidget {
             "&source.type=" + info.type;
     }
 
-    /**
-     * Build tb widgets
-     */
-    build() {
-        this.containers.forEach((info) => {
-            var div = document.getElementById(info.container);
-            if(div) {
-                fetch(this.buildUrl(info)).then((response) => {
-                    response.json().then((data) => {
-                        this.buildWidget(div, data);
-                    }).catch((reason) =>{
-                        this.logError("Failed to parse widget data", reason);
-                    });
-                }).catch((reason) => {
-                    this.logError("Failed to load widget data", reason);
-                });
-            }
-        });
-    }
 
     /**
      * Build the specific widget
      * @param {element} div 
      * @param {json} data 
      */
-    buildWidget(div, data){
+    buildWidget(div, data, template){
         var list = data.list;
         if(list) {
             var content = '';
             list.forEach((item) => {
-                var itemcontent = "<div class='tbitem'>";
-                itemcontent += "<a href='"+item.url+"' target='_blank'>";
-                itemcontent += "<div class='tbimage' style='background-image:url(\"" + item.thumbnail[0].url +"\")'>";
-                itemcontent += "</div>";
-                itemcontent += "<div class='tblabel'>";
-                itemcontent += item.name;
-                itemcontent += "</div>";
-                itemcontent += "</div>";
-                itemcontent += "</a>";
-                itemcontent += "</div>";
-
-                content += itemcontent;
+                content += template(this.buildItemInfo(item));
             });
             div.innerHTML = content;
         } else {
 
         }
+    }
+
+    /**
+     * Normalizes item info to hide JSON implementation
+     * @param {object} item 
+     */
+    buildItemInfo(item) {
+        return {
+            url:item.url,
+            thumbnail:item.thumbnail[0].url,
+            name:item.name
+        };
+    }
+
+    /**
+     * Gets an HTML item box from item object
+     * @param {object} item 
+     */
+    buildItem(item) {
+        return  "<div class='tbitem'>" +
+                    "<a href='"+item.url+"' target='_blank'>" +
+                        "<div class='tbimage' style='background-image:url(\"" + item.thumbnail +"\")'>" +
+                        "</div>" +
+                        "<div class='tblabel'>" +
+                            item.name +
+                        "</div>" +
+                        "</div>" +
+                    "</a>" +
+                "</div>";
     }
 
     /**
